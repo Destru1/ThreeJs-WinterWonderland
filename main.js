fd3943a2ef83f5124a5208a7d1fa6c17abed25ef
Nikolay Nikolaev <nikolay.p.nikolaev01@gmail.com>
2023-12-09 19:52:03 +0200

    Install three js

---
+++ main.js
@@ -0,0 +1,30 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+
+camera.position.z = 12
+camera.position.x = 2
+
+const ambientLight = new THREE.AmbientLight(0x414141); // soft white light
+scene.add(ambientLight);
+
+
+function animate(){
+    requestAnimationFrame(animate)
+
+
+    renderer.render(scene,camera)
+}
+animate()
